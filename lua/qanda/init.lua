7d6fb079d92455f3a6f11da31de1b663441ace18
Stuart Rackham <srackham@gmail.com>
2026-09-15 20:06:39 +1200

    add data directory to /status command output and drop session file

--- lua/qanda/init.lua
+++ lua/qanda/init.lua
@@ -187,7 +187,7 @@ Press <Tab> for command completion e.g. :Qanda /<Tab> to list builtin commands.
       else
         info = info .. "nil"
       end
-      info = info .. "\nsession file: " .. vim.inspect(Config.session_file())
+      info = info .. "\ndata directory: " .. vim.inspect(Config.data_dir)
       info = info .. "\nchats directory: " .. vim.inspect(Config.chats_dir)
       info = info .. "\nprompts directory: " .. vim.inspect(Config.prompts_dir)
       utils.notify(info, vim.log.levels.INFO)
