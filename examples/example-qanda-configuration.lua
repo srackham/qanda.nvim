487a2a0a248cd30cdeab3b50f88ac6c07c76b0fd
Stuart Rackham <srackham@gmail.com>
2026-09-20 09:29:37 +1200

    update example configuration

--- examples/example-qanda-configuration.lua
+++ examples/example-qanda-configuration.lua
@@ -15,47 +15,27 @@ return {
       provider_options = {
         ollama = { temperature = 0.4 },
       },
-      confirm_chat_file_deletion = false,
     }
 
     -- Key mappings for builtin commands --
-    vim.keymap.set({ "n", "v" }, "<S-Tab>", "<Cmd>Qanda /chat_window<CR>", { desc = "Qanda.nvim open user Chat window" })
-    vim.keymap.set({ "n", "v" }, "<Leader>lq", "<Cmd>Qanda /prompt_window<CR>", { desc = "Qanda.nvim open Prompt window" })
-    vim.keymap.set({ "n", "v", "i" }, "<C-Del>", "<Cmd>Qanda /new_prompt<CR>", { desc = "Qanda.nvim open new prompt" })
-    vim.keymap.set(
-      { "n", "v" },
-      "<Leader>lp",
-      "<Cmd>Qanda /prompt_template_picker<CR>",
-      { desc = "Qanda.nvim open prompts template picker" }
-    )
-    vim.keymap.set({ "n", "v" }, "<Leader>la", "<Cmd>Qanda /chat_window<CR>", { desc = "Qanda.nvim open Chat window" })
-    vim.keymap.set({ "n", "v" }, "<Leader>lc", "<Cmd>Qanda /chat_picker<CR>", { desc = "Qanda.nvim open Chat picker" })
-    vim.keymap.set({ "n", "v" }, "<Leader>ln", "<Cmd>Qanda /new_chat<CR>", { desc = "Qanda.nvim new chat" })
-    vim.keymap.set(
-      { "n", "v" },
-      "<Leader>ls",
-      "<Cmd>Qanda /system_template_picker<CR>",
-      { desc = "Qanda.nvim open System template picker" }
-    )
-    vim.keymap.set({ "n", "v" }, "<leader>lm", "<Cmd>Qanda /model_picker<CR>", { desc = "Qanda.nvim model selection" })
-    vim.keymap.set({ "n", "v" }, "<leader>lP", "<Cmd>Qanda /provider_picker<CR>", { desc = "Qanda.nvim provider selection" })
-    vim.keymap.set({ "n", "v" }, "<leader>lr", "<Cmd>Qanda /recent_models<CR>", { desc = "Qanda.nvim recent model selection" })
-    vim.keymap.set({ "n", "v" }, "<leader>li", "<Cmd>Qanda /status<CR>", { desc = "Qanda.nvim status information" })
-    vim.keymap.set({ "n", "v" }, "<leader>lk", "<Cmd>Qanda /abort<CR>", { desc = "Qanda.nvim abort the current request" })
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>ld",
-      "<Cmd>Qanda /diagnostics<CR>",
-      { desc = "Qanda.nvim display request/response diagnostics" }
-    )
-    vim.keymap.set({ "n", "v" }, "<leader>lt", "<Cmd>Qanda /turn_picker<CR>", { desc = "Qanda.nvim open turn picker" })
+    vim.keymap.set({ "n", "v" }, "<S-Tab>", "<Cmd>Qanda /chat_window<CR>", { desc = "Qanda.nvim user Chat window" })
+    vim.keymap.set({ "n", "v", "i" }, "<C-Del>", "<Cmd>Qanda /new_prompt<CR>", { desc = "Qanda.nvim new prompt" })
+    vim.keymap.set({ "n", "v" }, "<Leader>at", "<Cmd>Qanda /turn_picker<CR>", { desc = "Qanda.nvim turn picker" })
+    vim.keymap.set({ "n", "v" }, "<Leader>apt", "<Cmd>Qanda /prompt_template_picker<CR>", { desc = "Qanda.nvim prompts template picker" })
+    vim.keymap.set({ "n", "v" }, "<Leader>acp", "<Cmd>Qanda /chat_picker<CR>", { desc = "Qanda.nvim Chat picker" })
+    vim.keymap.set({ "n", "v" }, "<Leader>apw", "<Cmd>Qanda /prompt_window<CR>", { desc = "Qanda.nvim Prompt window" })
+    vim.keymap.set({ "n", "v" }, "<Leader>anc", "<Cmd>Qanda /new_chat<CR>", { desc = "Qanda.nvim new chat" })
+    vim.keymap.set({ "n", "v" }, "<leader>ams", "<Cmd>Qanda /model_picker<CR>", { desc = "Qanda.nvim model selection" })
+    vim.keymap.set({ "n", "v" }, "<leader>amp", "<Cmd>Qanda /provider_picker<CR>", { desc = "Qanda.nvim provider selection" })
+    vim.keymap.set({ "n", "v" }, "<leader>amr", "<Cmd>Qanda /recent_models<CR>", { desc = "Qanda.nvim recent model selection" })
+    vim.keymap.set({ "n", "v" }, "<leader>ai", "<Cmd>Qanda /status<CR>", { desc = "Qanda.nvim status information" })
+    vim.keymap.set({ "n", "v" }, "<leader>ak", "<Cmd>Qanda /abort<CR>", { desc = "Qanda.nvim abort the current request" })
 
     -- Key mappings for the default prompt templates --
-    vim.keymap.set({ "n", "v" }, "<C-q>", ":Qanda !Query<CR>", { desc = "Qanda.nvim ask a question" })
-    vim.keymap.set({ "n", "v" }, "<Leader>ad", ":Qanda !Dictionary definition<CR>", { desc = "Qanda.nvim dictionary definition" })
-    vim.keymap.set({ "n", "v" }, "<Leader>aS", ":Qanda !Spell a word<CR>", { desc = "Qanda.nvim spell a word" })
-    vim.keymap.set({ "n", "v" }, "<Leader>as", ":Qanda !Synonyms<CR>", { desc = "Qanda.nvim synonyms for word" })
-    vim.keymap.set({ "n", "v" }, "<Leader>aa", ":Qanda !Antonyms<CR>", { desc = "Qanda.nvim antonyms for word" })
+    vim.keymap.set({ "n", "v" }, "<Leader>aq", ":Qanda !Query<CR>", { desc = "Qanda.nvim ask a question" })
+    vim.keymap.set({ "n", "v" }, "<Leader>add", ":Qanda !Dictionary definition<CR>", { desc = "Qanda.nvim dictionary definition" })
+    vim.keymap.set({ "n", "v" }, "<Leader>ads", ":Qanda !Synonyms<CR>", { desc = "Qanda.nvim synonyms for word" })
+    vim.keymap.set({ "n", "v" }, "<Leader>ada", ":Qanda !Antonyms<CR>", { desc = "Qanda.nvim antonyms for word" })
 
   end,
 }
